78c8c257be4b04c1ed20df4df0f9bf80b2a1534b
andreimihai2305 <andreimihai2305is@gmail.com>
2024-08-26 11:50:23 +0300

    added multiple connections handling

--- client.py
+++ client.py
@@ -9,7 +9,15 @@ PORT = 9090
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((HOST, PORT))
+while True:
+    try:
+        mess = input("Message: ")
+        client.send(mess.encode("utf-8"))
+        print(f"Message sent to server: {mess}")
 
-data = input("Message: ")
-client.send(data.encode("utf-8"))
+    except:
+        client.close()
+        print("Client closed!")
+        break
+
 client.close()
--- server.py
+++ server.py
@@ -1,18 +1,38 @@
 import socket
+import threading
 
-HOST = socket.gethostbyname("192.168.100.9")
-#HOST = socket.gethostbyname("192.168.100.14") 
-
+HOST = socket.gethostbyname(socket.gethostname())
+print(HOST)
 PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((HOST, PORT))
 
+def handle_client(client, addr):
+    while True:
+        try:
+            mess = client.recv(1024)
+            if not mess:
+                client.close()
+                break
+            elif mess == "quit":
+                client.close()
+                break
+            else:
+                print(mess.decode(), client)
+
+        except:
+            print(f"Client {addr} disconnected!")
+            client.close()
+            break
+
+
 server.listen(5)
 print(f"Starting server on port: {PORT}")
 while True:
     client_socket, address = server.accept()
     print(address)
-    data = client_socket.recv(1024).decode()
-    print(f"Client said: {data}")
+    thread = threading.Thread(target=handle_client, args=(client_socket, address))
+    thread.start()
+
