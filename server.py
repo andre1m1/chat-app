f616c8d53aabf50747e2cb2af69e4a59dcd031d8
andreimihai2305 <andreimihai2305is@gmail.com>
2024-08-26 12:14:41 +0300

    keeping connections in server state

--- server.py
+++ server.py
@@ -6,33 +6,45 @@ print(HOST)
 PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((HOST, PORT))
 
-def handle_client(client, addr):
+def handle_client(client_dict):
+    client, addr = client_dict["conn"], client_dict["addr"]
     while True:
         try:
             mess = client.recv(1024)
+
             if not mess:
+                print(f"Client {addr} disconnected!")
                 client.close()
+                clients.remove(client_dict)
                 break
             elif mess == "quit":
+                print(f"Client {addr} disconnected!")
                 client.close()
+                clients.remove(client_dict)
                 break
             else:
-                print(mess.decode(), client)
+                print(mess.decode())
 
         except:
             print(f"Client {addr} disconnected!")
             client.close()
+            clients.remove(client_dict)
             break
 
 
+clients = []
 server.listen(5)
 print(f"Starting server on port: {PORT}")
 while True:
-    client_socket, address = server.accept()
-    print(address)
-    thread = threading.Thread(target=handle_client, args=(client_socket, address))
+    client_sock, addr = server.accept()
+    print(f"Client: {addr} connected!")
+    client = {"id" : len(clients), 
+              "conn": client_sock,
+              "addr": addr
+              }
+    clients.append(client)
+    thread = threading.Thread(target=handle_client, args=(client, ))
     thread.start()
 
